2abe4b7a30061e2bdc8516c8097a42c43059546e
Sougata Pal <palsougata256@gmail.com>
2025-04-04 15:06:40 +0530

    modified:   admin/landing/footerfile.php

--- admin/landing/footerfile.php
+++ admin/landing/footerfile.php
@@ -4,7 +4,7 @@
     <div class=" bg-green-400 h-auto flex flex-wrap gap-4 w-screen justify-between p-4">
         <div class=" flex flex-wrap flex-col gap-3">
             <img src="./landing/incognito_learner.png" alt="Company Logo" class=" h-20 w-20">
-            <h1 class=" text-10  font-bold">During this era, of online learning Incognito Helps you to achive your goals</h1>
+            <h1 class=" text-10  font-bold">During this era, of online learning Incognito Learning helps you to achive your goals</h1>
             <div class=" flex flex-wrap flex-row gap-10">
                 <a href="" class=" h-10 bg-red-300 ww-[10px] p-3 justify-center items-center text-center rounded-3xl flex">Google Play</a>
                 <a href="" class=" h-10 bg-red-300 ww-[10px] p-3 justify-center items-center text-center rounded-3xl flex">App Store</a>
@@ -34,7 +34,7 @@
         class="h-10 w-full pl-3 pr-16 rounded-md bg-slate-200 text-black focus:outline-none"
     >
     <button 
-        class="absolute right-0 top-0 h-10 px-3 bg-green-500 text-white rounded-md hover:bg-blue-600"
+        class="absolute right-0 top-0 h-10 px-3 bg-green-500 text-white rounded-md hover:bg-green-800"
     >
         Subscribe
     </button>
@@ -45,7 +45,7 @@
     </div>
     <!-- fourth -->
     <div class=" justify-between bg-green-700  text-white flex flex-wrap flex-row w-screen h-10 items-center text-center    ">
-        <h1>Privacy Policy|Terms & Conditions</h1>
-        <h1>Copyright C by Incognito Learning</h1>
+        <h1>Privacy Policy | Terms & Conditions</h1>
+        <h1>Copyright © by Incognito Learning</h1>
     </div>
 </div>
